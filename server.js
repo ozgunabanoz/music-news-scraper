131dd41782d32be9ef1bf5f4c470eea2bdf2cc3b
Ozgun <ozgun.abanoz@gmail.com>
2019-06-21 21:21:48 +0300

    Interval little fix

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 require('./routes/apiRoutes')(app);
 
 newsFeedCallOperator(); // first function call independent of setinterval
-setInterval(() => newsFeedCallOperator(), 59 * 60 * 1000); // calling the function every 59 minutes
+setInterval(() => newsFeedCallOperator(), 20 * 60 * 1000); // calling the function every 20 minutes
 
 app.listen(process.env.PORT, () => {
     console.log(`Server listening at ${process.env.PORT}`);
